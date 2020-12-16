cca6eef00cc4293a2af6d61b93899a9f4742c510
Nikita Shubin <50959311+YourMonika@users.noreply.github.com>
2020-12-17 02:40:02 +0300

    prepon

---
+++ TwoFish/algorithms.hpp
@@ -0,0 +1,18 @@
+//
+// Created by MI on 16/12/2020
+//
+#ifndef TWOFHISH_ALGORITHMS_HPP
+#define TWOFHISH_ALGORITHMS_HPP
+
+#include <array>
+#include <utility>
+
+namespace twofish{
+    using namespace std;
+
+
+
+
+}
+
+#endif TWOFHISH_ALGORITHMS_HPP
---
+++ TwoFish/cmake-build-debug/CMakeFiles/TwoFish.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TwoFish.dir/main.cpp.o"
+  "TwoFish.exe"
+  "TwoFish.pdb"
+  "libTwoFish.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TwoFish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
